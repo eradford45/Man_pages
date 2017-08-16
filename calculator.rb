c83fe4dab7306f03ce44b4e2b0a9530ef16c1f58
Evan Radford <Eradford45@gmail.com>
2017-08-16 15:15:21 -0600

    finished basic calculator`

--- calculator.rb
+++ calculator.rb
@@ -16,34 +16,34 @@ def calculations(first_num, second_num, operator)
     case operator
         when operator = "+"
             first_num = (first_num + second_num)
-            puts first_num
-            menu2
+            puts "the anwser is #{first_num}"
+            menu2(first_num)
         when operator = "-"
             first_num = (first_num - second_num)
-            p first_num
-            menu2
+            puts "the anwser is #{first_num}"
+            menu2(first_num)
         when operator = "/"
             first_num = (first_num / second_num)
-            p first_num
-            menu2
+            puts "the anwser is #{first_num}"
+            menu2(first_num)
         when operator = "*"
             first_num = (first_num * second_num)
-            p first_num
-            menu2
+            puts "the anwser is #{first_num}"
+            menu2(first_num)
         else
             p "#{operator} is not supported please enter +, -, /, *"
             operator = gets.strip
     end
 end
 
-def menu2
+def menu2(first_num)
     p 'Choose one of the following options'
     p '1) continue with current number'
-    p '2) choose new number'
+    p '2) clear' 
     p '3) exit'
     user_input = gets.strip.to_i
     if user_input == 1
-        menu3
+        menu3(first_num)
     elsif user_input == 2
         menu
     elsif user_input == 3
@@ -53,6 +53,14 @@ def menu2
         menu2           
     end 
 end   
+
+def menu3(first_num)
+    p 'enter second number'
+    second_num = gets.strip.to_i
+    p 'enter operator +, -, /, *'
+    operator = gets.strip
+    calculations(first_num, second_num, operator)
+end
 
 
 menu
