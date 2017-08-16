cf1839b2ec3b0cee51931d27b70ae74c02d4ca19
Evan Radford <Eradford45@gmail.com>
2017-08-16 14:32:22 -0600

    Calculator works (needs looping)

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,28 @@
 #make sure all requires are at the top
 require 'pry'
 
-p 'welcome to the ruby calculator'
 
-# todo make a calculator!
+    p 'welcome to the ruby calculator'
+    p 'please enter the first number'
+    first_num = gets.strip.to_i
+    p 'please enter the second number'
+    second_num = gets.strip.to_i
+    p 'please enter + - / *'
+    operator = gets.strip
+case operator
+    when operator = "+"
+        third_num = (first_num + second_num)
+        puts third_num
+    when operator = "-"
+        third_num = (first_num - second_num)
+        p third_num
+    when operator = "/"
+        third_num = (first_num / second_num)
+        p third_num
+    when operator = "*"
+        third_num = (first_num * second_num)
+        p third_num
+    else
+        p "#{operator} is not supported please enter +, -, /, *"
+end
+
