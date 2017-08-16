c2be8d1fbdeea463c6fd164f54a0deebbb2f7eae
Evan Radford <Eradford45@gmail.com>
2017-08-16 14:52:49 -0600

    loop worksish

--- calculator.rb
+++ calculator.rb
@@ -15,21 +15,45 @@ end
 def calculations(first_num, second_num, operator)
     case operator
         when operator = "+"
-            third_num = (first_num + second_num)
-            puts third_num
+            first_num = (first_num + second_num)
+            puts first_num
+            menu2
         when operator = "-"
-            third_num = (first_num - second_num)
-            p third_num
+            first_num = (first_num - second_num)
+            p first_num
+            menu2
         when operator = "/"
-            third_num = (first_num / second_num)
-            p third_num
+            first_num = (first_num / second_num)
+            p first_num
+            menu2
         when operator = "*"
-            third_num = (first_num * second_num)
-            p third_num
+            first_num = (first_num * second_num)
+            p first_num
+            menu2
         else
             p "#{operator} is not supported please enter +, -, /, *"
+            operator = gets.strip
     end
 end
+
+def menu2
+    p 'Choose one of the following options'
+    p '1) continue with current number'
+    p '2) choose new number'
+    p '3) exit'
+    user_input = gets.strip.to_i
+    if user_input == 1
+        menu3
+    elsif user_input == 2
+        menu
+    elsif user_input == 3
+        exit
+    else
+        p 'please choose 1, 2 or 3'
+        menu2           
+    end 
+end   
+
 
 menu
 
